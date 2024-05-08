4beccd13de1eac14fa2a1112827b1c4deb720d65
소빈 <sobin11@naver.com>
2024-05-08 16:48:00 +0900

    ch7 과제 제출

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const cors = require('cors');
-
+const connectWithDB = require('./config/db');
+const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 const app = express();
 
+connectWithDB();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
 app.use(cookieParser());
+
 app.use(cookieSession({
     name: 'session',
     maxAge: process.env.COOKIE_TIME * 24 * 60 * 60 * 1000,
@@ -22,9 +33,13 @@ app.use(cors({
     credentials: true
 }))
 
-app.listen(process.env.PORT || 8000, (err)=>{
-    if(err){
-        console.log('Error in connecting to server : ' + err)
+
+app.use('/', require('./routes'));
+
+
+app.listen(process.env.PORT || 8000, (err) => {
+    if (err) {
+        console.log('Error in connecting to server: ' + err);
     }
 
     console.log(`Server is running on port ${process.env.PORT}`)
